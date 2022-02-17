f04c7974b7fc802aa3c284496f035b3f05f90d94
Allen1918 <2020302191891@whu.edu.cn>
2022-02-17 16:27:05 +0800

    2-17

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -16,7 +16,7 @@ function config.catppuccin()
         term_colors = true,
         styles = {
             comments = "italic",
-            functions = "italic,bold",
+            functions = "italic",
             keywords = "italic",
             strings = "NONE",
             variables = "NONE"
@@ -39,7 +39,7 @@ function config.catppuccin()
                 }
             },
             lsp_trouble = true,
-            lsp_saga = true,
+            lsp_saga = false,
             gitgutter = false,
             gitsigns = true,
             telescope = true,
@@ -55,7 +55,8 @@ function config.catppuccin()
             markdown = true,
             lightspeed = false,
             ts_rainbow = true,
-            hop = true
+            hop = true,
+            cmp = true
         }
     })
 end
@@ -221,51 +222,51 @@ function config.nvim_tree()
             height = 30,
             hide_root_folder = false,
             side = "left",
-            auto_resize = false,
-            mappings = {custom_only = false, list = {}},
+            auto_resize = true,
             number = false,
             relativenumber = false,
-            signcolumn = "yes"
-        },
-        mappings = {
-            custom_only = true,
-            list = {
-                {key = {"<CR>", "o", "<2-LeftMouse>"}, cb = tree_cb("edit")},
-                {key = {"<2-RightMouse>", "<C-]>"}, cb = tree_cb("cd")},
-                {key = "<C-v>", cb = tree_cb("vsplit")},
-                {key = "<C-x>", cb = tree_cb("split")},
-                {key = "<C-t>", cb = tree_cb("tabnew")},
-                {key = "<", cb = tree_cb("prev_sibling")},
-                {key = ">", cb = tree_cb("next_sibling")},
-                {key = "P", cb = tree_cb("parent_node")},
-                {key = "<BS>", cb = tree_cb("close_node")},
-                {key = "<S-CR>", cb = tree_cb("close_node")},
-                {key = "<Tab>", cb = tree_cb("preview")},
-                {key = "K", cb = tree_cb("first_sibling")},
-                {key = "J", cb = tree_cb("last_sibling")},
-                {key = "I", cb = tree_cb("toggle_ignored")},
-                {key = "H", cb = tree_cb("toggle_dotfiles")},
-                {key = "R", cb = tree_cb("refresh")},
-                {key = "m", cb = tree_cb("create")},
-                {key = "d", cb = tree_cb("remove")},
-                {key = "a", cb = tree_cb("rename")},
-                {key = "<C-r>", cb = tree_cb("full_rename")},
-                {key = "x", cb = tree_cb("cut")},
-                {key = "yy", cb = tree_cb("copy")},
-                {key = "p", cb = tree_cb("paste")},
-                {key = "yn", cb = tree_cb("copy_name")},
-                {key = "yp", cb = tree_cb("copy_path")},
-                {key = "gy", cb = tree_cb("copy_absolute_path")},
-                {key = "[c", cb = tree_cb("prev_git_item")},
-                {key = "]c", cb = tree_cb("next_git_item")},
-                {key = "-", cb = tree_cb("dir_up")},
-                {key = "s", cb = tree_cb("system_open")},
-                {key = "q", cb = tree_cb("close")},
-                {key = "?", cb = tree_cb("toggle_help")}
+            signcolumn = "yes",
+            mappings = {
+                custom_only = true,
+                list = {
+                    {key = {"<CR>", "o", "<2-LeftMouse>"}, cb = tree_cb("edit")},
+                    {key = {"<2-RightMouse>", "<C-]>"}, cb = tree_cb("cd")},
+                    {key = "<C-v>", cb = tree_cb("vsplit")},
+                    {key = "<C-x>", cb = tree_cb("split")},
+                    {key = "<C-t>", cb = tree_cb("tabnew")},
+                    {key = "<", cb = tree_cb("prev_sibling")},
+                    {key = ">", cb = tree_cb("next_sibling")},
+                    {key = "P", cb = tree_cb("parent_node")},
+                    {key = "<BS>", cb = tree_cb("close_node")},
+                    {key = "<S-CR>", cb = tree_cb("close_node")},
+                    {key = "<Tab>", cb = tree_cb("preview")},
+                    {key = "K", cb = tree_cb("first_sibling")},
+                    {key = "J", cb = tree_cb("last_sibling")},
+                    {key = "I", cb = tree_cb("toggle_ignored")},
+                    {key = "H", cb = tree_cb("toggle_dotfiles")},
+                    {key = "R", cb = tree_cb("refresh")},
+                    {key = "m", cb = tree_cb("create")},
+                    {key = "d", cb = tree_cb("remove")},
+                    {key = "a", cb = tree_cb("rename")},
+                    {key = "<C-r>", cb = tree_cb("full_rename")},
+                    {key = "x", cb = tree_cb("cut")},
+                    {key = "yy", cb = tree_cb("copy")},
+                    {key = "p", cb = tree_cb("paste")},
+                    {key = "yn", cb = tree_cb("copy_name")},
+                    {key = "yp", cb = tree_cb("copy_path")},
+                    {key = "gy", cb = tree_cb("copy_absolute_path")},
+                    {key = "[c", cb = tree_cb("prev_git_item")},
+                    {key = "]c", cb = tree_cb("next_git_item")},
+                    {key = "-", cb = tree_cb("dir_up")},
+                    {key = "s", cb = tree_cb("system_open")},
+                    {key = "q", cb = tree_cb("close")},
+                    {key = "?", cb = tree_cb("toggle_help")}
+                }
             }
         },
         trash = {cmd = "trash", require_confirm = true}
     }
+    vim.g.nvim_tree_indent_markers = 1
 end
 
 function config.nvim_bufferline()
