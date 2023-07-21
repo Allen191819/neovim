bcba491b698ab819dd95f8680675bbd03d96cd02
Allen191819 <2020302191891@whu.edu.cn>
2023-07-21 15:39:38 +0800

    07-21-2023 new features!!!

--- lua/modules/configs/completion/servers/html.lua
+++ lua/modules/configs/completion/servers/html.lua
@@ -1,6 +1,6 @@
 -- https://github.com/vscode-langservers/vscode-html-languageserver-bin
 return {
-	cmd = { "html-languageserver", "--stdio" },
+	cmd = { "vscode-html-languageserver", "--stdio" },
 	filetypes = { "html" },
 	init_options = {
 		configurationSection = { "html", "css", "javascript" },
